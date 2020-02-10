7e1170e2636dba1fc47620755950473713c60ba4
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-02-10 10:39:59 -0300

    Created the script PlayerAction and added movementation to Player

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+public class PlayerAction : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 50f;
+
+    Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        KeyboardMovement();
+
+    }
+    void KeyboardMovement()
+    {
+        var translation = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        var straffe = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+    }
+}
