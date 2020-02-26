0a3e6fcef1d61883951deb86884b8a8bb9787419
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-02-25 21:50:49 -0300

    Added knockback after enemy attack

--- Assets/Scripts/EnemyAction.cs
+++ Assets/Scripts/EnemyAction.cs
@@ -1,23 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAction : MonoBehaviour
 {
     public int damage = 1;
+    public float attackKnockback = 5f;
 
     NavMeshAgent agent;
     GameObject player;
+    Rigidbody rb;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("player");
         agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
 
         InvokeRepeating("GoToPlayer", 0, 1);
     }
 
     void GoToPlayer() {
         agent.destination = player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        LookAtPlayer();
+    }
+
+    void LookAtPlayer()
+    {
         transform.LookAt(player.transform.position, Vector3.up);
     }
 
@@ -27,6 +41,8 @@ public class EnemyAction : MonoBehaviour
         {
             var playerHealth = collision.gameObject.GetComponent<HealthAction>();
             playerHealth.ChangeHealth(-this.damage);
+            LookAtPlayer();
+            rb.AddForce(Vector3.back * attackKnockback, ForceMode.Impulse);
         }
     }
 }
